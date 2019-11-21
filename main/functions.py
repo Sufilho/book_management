484a0ae6d99151bb3a616c2edadf44e7468ae93b
Sufilho <sufilho@users.noreply.github.com>
2019-11-21 16:35:51 -0300

    Still trying to use json to keep book data

--- main/functions.py
+++ main/functions.py
@@ -1,16 +1,14 @@
-""" 
-• Ao executar o sistema deve mostrar um menu para o usuário digitar quais operação deseja
-realizar.
-• Cadastrar ITEM
-• Alterar ITEM
-• Excluir ITEM fornecendo o Código
-• Localizar um ITEM pelo Código
-• Listagem geral dos ITEMS (apresentando a quantidade geral, e o somatório / media dos
-valores numéricos). 
-• Item escolhido -> LIVROS (Código, Nome, Editora, Ano, ValorPago).
-"""
+# • Ao executar o sistema deve mostrar um menu para o usuário digitar quais operação deseja
+# realizar.
+# • Cadastrar ITEM
+# • Alterar ITEM
+# • Excluir ITEM fornecendo o Código
+# • Localizar um ITEM pelo Código
+# • Listagem geral dos ITEMS (apresentando a quantidade geral, e o somatório / media dos
+# valores numéricos). 
+# • Item escolhido -> LIVROS (Código, Nome, Editora, Ano, ValorPago).
 
-
+import json
 
 class Book:
     def __init__(self, code, name, publisher, year, price):
@@ -19,8 +17,6 @@ class Book:
         self.publisher = publisher
         self.year = year
         self.price = price
-
-
 
 def option_check(option):
     if option not in [1,2,3,4,5]:
@@ -36,27 +32,31 @@ def register_book():
     publisher = input('NOME DA EDITORA: ').upper()
     year = int(input('ANO DE PUBLICAÇÃO: '))
     price = float(input('PREÇO DO LIVRO: '))
-
-
-    book = Book(code,name,publisher,year,price)
-    book_list = [book.code, book.name, book.publisher, book.year, book.price]
-    print(book_list)
     
-    with open("./data.txt",'a') as f:
-        f.write(str(book_list))    
-        f.write('\n')
+    book = Book(code,name,publisher,year,price)
+    book_list = {
+                'Código': book.code, 
+                'Nome': book.name, 
+                'Editora': book.publisher, 
+                'Ano': book.year, 
+                'Preço': book.price
+                }
+    print(book_list)
+
+    # with open("./data.txt",'a') as f:
+    #     f.write(str(book_list))    
+    #     f.write('\n')
 
     print('\n|CADASTRO EFETUADO COM SUCESSO!|\n')
 
-""" def change_record():
+# def change_record():
 
-def remove_book():
+# def remove_book():
 
-def find_book():
+# def find_book():
 
-def list_books(): 
+# def list_books(): 
 
-""" 
 def menu():
     print('\n[1] - CADASTRAR LIVRO \n[2] - ALTERAR CADSTRO DE LIVRO \n[3] - EXCLUIR CADASTRO DE LIVRO PELO CÓDIGO \n[4] - LOCALIZAR LIVRO PELO CÓDIGO \n[5] - LISTAR ITENS')
     option = int(input('Digite qual operação você deseja fazer: '))
