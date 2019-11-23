8e6260657d6b0cc988d5acffe97386b82fa3fcc7
Sufilho <filho.ers@gmail.com>
2019-11-23 02:50:49 -0300

    Progressing with .json to keep data

--- App/main/functions.py
+++ App/main/functions.py
@@ -22,8 +22,7 @@ def option_check(option):
     if option not in [1,2,3,4,5]:
         print('[ERRO] - OPÇÃO INVÁLIDA!')
         return menu()
-    elif option == 1:
-        return register_book()
+    
 
 def register_book():
     print('\nCADASTRO DE LIVRO\n')
@@ -34,29 +33,30 @@ def register_book():
     price = float(input('PREÇO DO LIVRO: '))
     
     book = Book(code,name,publisher,year,price)
-    book_list = {
-                'Código': book.code, 
-                'Nome': book.name, 
-                'Editora': book.publisher, 
-                'Ano': book.year, 
-                'Preço': book.price
-                }
-    print(book_list)
-    
-    # This is used to mannage a .json file content
-    # with open('data.json') as f:
-    #     data = json.load(f)
-    # for state in data['states']:
-    #     print(state['name'])
 
-    # This was use to write data on a .txt file
-    # with open("./data.txt",'a') as f:
-    #     f.write(str(book_list))    
-    #     f.write('\n')
+    book_dict = {
+        'code': book.code, 
+        'name': book.name, 
+        'publisher': book.publisher, 
+        'year': book.year, 
+        'price': book.price
+    }
+
+    print(type(book_dict))
+    to_json = json.dumps((book_dict),indent=4)
+    with open("data.json","a") as f:
+        data = f.write(to_json)
+        f.write('\n')
 
     print('\n|CADASTRO EFETUADO COM SUCESSO!|\n')
 
-# def change_record():
+def change_record():
+    with open('data.json','r') as f:
+        from_json = f.read()
+    print(from_json)
+    data = json.loads(from_json)
+    print(data['code'])
+    
 
 # def remove_book():
 
@@ -68,3 +68,8 @@ def menu():
     print('\n[1] - CADASTRAR LIVRO \n[2] - ALTERAR CADSTRO DE LIVRO \n[3] - EXCLUIR CADASTRO DE LIVRO PELO CÓDIGO \n[4] - LOCALIZAR LIVRO PELO CÓDIGO \n[5] - LISTAR ITENS')
     option = int(input('Digite qual operação você deseja fazer: '))
     option_check(option)
+    if option == 1:
+        return register_book()
+    elif option == 2:
+        # code = input('Digite o código do livro que você deseja alterar o cadastro: ') 
+        change_record()
